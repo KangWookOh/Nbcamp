c6512fc4aa05caacb9c17ce5156ae2a56b36e70c
ohkangwook <kanguk456@gmail.com>
2024-07-24 19:09:44 +0900

    2-1

---
+++ CalculationException.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class CalculationException extends RuntimeException{
+    public CalculationException(String message){
+        super(message);
+    }
+}
--- Calculator.java
+++ Calculator.java
@@ -25,6 +25,9 @@ public class Calculator {
                      throw  new IllegalStateException("분모가 0이되면 안됩니다.");
                  }
                  result=  num1 /num2;
+                 break;
+             default:
+                 throw new CalculationException("올바른 연산 기호를 입력하세요");
          }
          results.add(result);
          return result;
