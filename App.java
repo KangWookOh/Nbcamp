592adfbb08064af532fb71b3942dacf104b1be7c
ohkangwook <kanguk456@gmail.com>
2024-07-25 15:25:00 +0900

    2-7

--- App.java
+++ App.java
@@ -9,31 +9,66 @@ public class App {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
+
         Scanner sc = new Scanner(System.in);
 
         while (true){
-            System.out.println("첫번쨰 숫자를 입력하세요: ");
-            int input1 = sc.nextInt(); // 첫번째 input 받기
-            System.out.println("두번째 숫자를 입력하세요: ");
-            int input2 = sc.nextInt(); // 첫번째 input 받기
-            System.out.println("사칙연산 기호를 선택해 주세요(+,-,*,/)");
-            char sign = sc.next().charAt(0); // 연산자 받기
-            int result = 0;
-            calculator.calculate(input1, input2, sign);
+            System.out.println("사칙연산을 진행하시겠습니까? 원의 넓이를 구하시겠습니까?(사칙연산: calc 원의 넓이 : circle)");
+            String mode = sc.next();
+            if(mode.equals("calc")){
+                System.out.println("첫번쨰 숫자를 입력하세요: ");
+                int input1 = sc.nextInt(); // 첫번째 input 받기
 
-            System.out.println("결과: "+calculator.result);
+                System.out.println("두번째 숫자를 입력하세요: ");
+                int input2 = sc.nextInt(); // 첫번째 input 받기
+                System.out.println("사칙 연산 기호를 입력 하세요: ");
+                char sign = sc.next().charAt(0); // 연산자 받기
+                try {
+                    int result = calculator.calculate(input1, input2, sign);
+                    System.out.println("결과: "+ calculator.result);
+                }catch (CalculationException e){
+                    System.out.println(e.getMessage());
+                }
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료, remove 입력 시 가장 먼저 저장된 연산 결과 삭제, inquiry 입력 시 모든 연산 결과 조회)");
+                String question = sc.next();
+                if(question.equals("exit")) {
+                    break;
+                }
+                else if(question.equals("remove")) {
+                    calculator.remove();
+                    continue;
+                }
+                else if(question.equals("inquiry")){
+                    calculator.inquiry();
+                    continue;
+                }
+
+            }
+            else if(mode.equals("circle")){
+                System.out.print("원의 반지름을 입력하세요: ");
+                double radius = sc.nextDouble();
+                try {
+                    double area =calculator.calculateCircleArea(radius);
+                    System.out.println("원의 넓이 : "+area);
+                }
+                catch (CalculationException e){
+                    System.out.println(e.getMessage());
+                }
+            }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료, remove 입력 시 가장 먼저 저장된 연산 결과 삭제, inquiry 입력 시 모든 연산 결과 조회)");
-            String question = sc.next(); // 입력값을 받다 exit을 받았을 경우 실행을 멈추는 로직
-            if(question.equals("exit")) {
-                break;
+            String answer = sc.next();
+            if(answer.equals("exit")){
+               break;
             }
-            if(question.equals("remove")) {
-                calculator.remove();
+            else if(answer.equals("remove")){
+                calculator.circleRemove();
+                continue;
             }
-            else if(question.equals("inquiry")){
-               calculator.inquiry();
+            else if (answer.equals("inquiry")){
+                calculator.inquiryCricle();
+                continue;
             }
-        }
 
-    }
+        }
+}
 }
--- Calculator.java
+++ Calculator.java
@@ -8,12 +8,19 @@ import java.util.List;
 
 public class Calculator {
    private List<Integer> res ;
-   
+   private List<Double> circle;
+   private static final double pi = 3.14;
+   // static을 사용하게 되면 전역변수로써 사용이 가능하지만 상수가 아니므로 언제든 변경될수 있기때문에
+    // final을 사용해 해당 값이 저장되면 최종적인 값이 되므로 수정이 불가능하게 된다.
+
    public Calculator() {
        this.res = new ArrayList<>();
+       this.circle = new ArrayList<>();
    }
 
     int result=0;
+    double redius =0;
+
     public int calculate(int input1, int input2,char sign){
 
         switch(sign){
@@ -38,23 +45,49 @@ public class Calculator {
         res.add(result);
         return result;
     }
+    public double calculateCircleArea(double redius){
+        if(redius<0){
+            throw new CalculationException("반지름은 양수여야 합니다.");
+        }
+        double area = pi*redius*redius;
+        circle.add(area);
+        return  area;
+    }
     //getter 구현 메소드
     public List<Integer> getRes() {
         return new ArrayList<>(res);
     }
+    public List<Double> getCircle() {
+        return new ArrayList<>(circle);
+    }
     //setter 구현 메소드
     public void setRes(List<Integer> res) {
-        this.res = new ArrayList<>();
+        this.res = new ArrayList<>(res);
     }
+    public void setCircle(List<Double> circle) {
+        this.circle = new ArrayList<>(circle);
+    }
+
     public  void remove(){ // 삭제 메소드 구현
         if(!res.isEmpty()){
             res.remove(0);
             System.out.println("삭제 되었습니다.");
         }
     }
+    public void circleRemove(){
+        if(!circle.isEmpty()){
+            circle.remove(0);
+            System.out.println("삭제 되었습니다.");
+        }
+    }
     public void inquiry(){ // 조회 메소드 구현
         for(int total : res){
             System.out.println(total);
+        }
+    }
+    public void inquiryCricle(){ // 원의 넓이 조회 구현 메소드
+        for(double circleTotal : circle){
+            System.out.println(circleTotal);
         }
     }
 
