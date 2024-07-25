c1a75f8679f3d057f8eab1937d315126ce5200e1
ohkangwook <kanguk456@gmail.com>
2024-07-25 10:01:13 +0900

    2-2

--- App.java
+++ App.java
@@ -7,12 +7,8 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        // 10개를 저장할수 있는 배열 생성 및 초기화
-        List<Integer> results = new ArrayList<Integer>();
-        // 배열의 저장된 결과 값 세기
-        int count = 0;
-        //배열 에 저장할 인덱스
-        int index = 0;
+        Calculator calculator = new Calculator();
+
         Scanner sc = new Scanner(System.in);
 
         while (true){
@@ -23,46 +19,22 @@ public class App {
             System.out.println("사칙연산 기호를 선택해 주세요(+,-,*,/)");
             char sign = sc.next().charAt(0); // 연산자 받기
             int result = 0;
-            switch (sign) {
-                case '+':
-                    result = input1 + input2; // +연산
-                    break;
-                case '-':
-                    result = input1 - input2; //-연산
-                    break;
-                case '*':
-                    result = input1 * input2; //*연산
-                    break;
-                case '/':
-                    if(input2!=0){ // 조건문을 걸어 나누기 일경우 2번째 입력값이 0이 아닌경우 연산을 수행하고 0일경우 프린트문 출력후 리턴
-                        result = input1 / input2;
-                        //+연산
-                    }
-                    else{
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        continue;
-                    }
-                    break;
-                default:
-                    System.out.println("올바른 연산 기호를 입력하세요"); // 위의 사칙연산 문이 아닌 다른 연산문일시 계속 진행하는 로직
-                    continue;
-            }
-            results.add(result);
-            System.out.println("결과: "+result);
-
+            calculator.calculate(input1, input2, sign);
+            calculator.res.add(result);
+            System.out.println("결과: "+calculator.result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료, remove 입력 시 가장 먼저 저장된 연산 결과 삭제, inquiry 입력 시 모든 연산 결과 조회)");
             String question = sc.next(); // 입력값을 받다 exit을 받았을 경우 실행을 멈추는 로직
-            if(question.equalsIgnoreCase("exit")) {
+            if(question.equals("exit")) {
                 break;
             }
-            else if(question.equalsIgnoreCase("remove")){
-                if(!results.isEmpty()){
-                    results.remove(0);
+            else if(question.equals("remove")){
+                if(!calculator.res.isEmpty()){
+                    calculator.res.remove(0);
                     System.out.println("삭제 되었습니다.");
                 }
             }
-            else if(question.equalsIgnoreCase("inquiry")){
-                for(int res: results){
+            else if(question.equals("inquiry")){
+                for(int res: calculator.res){
                     System.out.println(res);
                 }
             }
--- Calculator.java
+++ Calculator.java
@@ -4,38 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-     List<Integer> results ;
-     public Calculator(){
-         this.results =new ArrayList<>();
-     }
-    int result =0;
-     public int calculate(int num1, int num2,char sign) {
-         switch (sign){
-             case '+':
-                 result = num1 + num2;
-                 break;
-             case '-':
-                 result =num1 - num2;
-                 break;
-             case '*':
-                 result =num1 * num2;
-                 break;
-             case '/':
-                 if(num2 == 0){
-                     throw  new IllegalStateException("분모가 0이되면 안됩니다.");
-                 }
-                 result=  num1 /num2;
-                 break;
-             default:
-                 throw new CalculationException("올바른 연산 기호를 입력하세요");
-         }
-         results.add(result);
-         return result;
+    List<Integer> res ;
+    public Calculator(){
+        res = new ArrayList<Integer>();
+    }
+    int result=0;
+    public int calculate(int input1, int input2,char sign){
 
-     }
-
-
-
-
+        switch(sign){
+            case '+':
+                result = input1 + input2;
+                break;
+            case '-':
+                result = input1 - input2;
+                break;
+            case '*':
+                result = input1 * input2;
+                break;
+            case '/':
+                if (input2 == 0) {
+                    throw new CalculationException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }
+                result = input1 / input2;
+                break;
+            default:
+                throw new CalculationException("올바른 연산기호를 입력하세요");
+        }
+        res.add(result);
+        return result;
+    }
 
 }
