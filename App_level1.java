fd14aebdc413d8cad2c08d2ce8c87efd8cabfad3
ohkangwook <kanguk456@gmail.com>
2024-07-26 15:00:49 +0900

    1-1

---
+++ App_level1.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class App_level1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int input1 =sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int input2 =sc.nextInt();
+
+
+
+    }
+}
