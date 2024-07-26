28c1a9f764a63c35b0e1a81c2eb21c928d24c20c
ohkangwook <kanguk456@gmail.com>
2024-07-26 16:10:22 +0900

    1-7

--- App_level1.java
+++ App_level1.java
@@ -1,11 +1,12 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App_level1 {
     public static void main(String[] args) {
-        int[] res =new int[10];
-        int index =0;
+        List<Integer> res = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
 
 
@@ -31,23 +32,23 @@ public class App_level1 {
                     result = input1 / input2;
                     break;
             }
+            res.add(result);
             System.out.println("결과: " + result);
-            //연산 결과 배열의 저장
-                if(index < res.length){ // 인데스가 배열의 길이 보다 작으면 인덱스 증가 수행
-                    res[index] = result;
-                    index++;
-                }
-                else {
-                    for(int i = index; i < res.length; i++){ // 배열이 가득 찬 경우 한칸씩 이동시키고 새로운 결과를 마지막에 저장
-                        res[i -i] =res[result];
-                    }
-                    res[res.length - 1] = result;
-                }
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.println("remove 입력시 가장 먼저 저장된 결과 삭제");
             String answer = sc.next(); // 입력값 받은후 해당 입력값이 if문 과 같다면 해당 if문 수행
             if(answer.equals("exit")){
                 break;
             }
+            else if(answer.equals("remove")){
+                if(!res.isEmpty()){
+                    res.remove(0);
+                    System.out.println("삭제 되었습니다");
+                    break;
+                }
+            }
+          
 
 
 
