d90ad506a98a1d467e06b8f89bd42f8c3bac84b6
ohkangwook <kanguk456@gmail.com>
2024-07-26 16:01:36 +0900

    1-6

--- App_level1.java
+++ App_level1.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 
 public class App_level1 {
     public static void main(String[] args) {
+        int[] res =new int[10];
+        int index =0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int input1 =sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int input2 =sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char sign = sc.next().charAt(0);
+
 
         int result = 0;
         while (true){
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int input1 =sc.nextInt(); // 첫번째 숫자 입력
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int input2 =sc.nextInt(); // 두번째 숫자 입력
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char sign = sc.next().charAt(0); // 문자열의 첫번쨰 문자 확인
             switch (sign) {
                 case'+':
                     result = input1 + input2;
@@ -29,11 +32,25 @@ public class App_level1 {
                     break;
             }
             System.out.println("결과: " + result);
+            //연산 결과 배열의 저장
+                if(index < res.length){ // 인데스가 배열의 길이 보다 작으면 인덱스 증가 수행
+                    res[index] = result;
+                    index++;
+                }
+                else {
+                    for(int i = index; i < res.length; i++){ // 배열이 가득 찬 경우 한칸씩 이동시키고 새로운 결과를 마지막에 저장
+                        res[i -i] =res[result];
+                    }
+                    res[res.length - 1] = result;
+                }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String answer = sc.next();
+            String answer = sc.next(); // 입력값 받은후 해당 입력값이 if문 과 같다면 해당 if문 수행
             if(answer.equals("exit")){
                 break;
             }
+
+
+
 
         }
 
